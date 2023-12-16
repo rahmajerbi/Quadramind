3ddc6f7cfa85e797cb2b432c8c30ae31f6e0b46a
Mahira Asadzade <56446463+MahireAsadzade@users.noreply.github.com>
2023-12-16 19:26:49 +0100

    Update KMeans anomaly detection

--- PySpark/KMeans.py
+++ PySpark/KMeans.py
@@ -2,10 +2,60 @@ from pyspark.sql import SparkSession
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.clustering import KMeans
 from pyspark.sql.functions import when, col
+from pyspark.ml.feature import VectorAssembler
 import matplotlib.pyplot as plt
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+import pyspark
+import os
 
-# Initialize Spark session
-spark = SparkSession.builder.appName("KMeansAnomalyDetection").getOrCreate()
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.5.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 pyspark-shell'
+
+topic_name = 'Topic_test'
+output_topic = 'Topic_KMeans'
+
+class BloodPressure:
+    
+    def get_schema():
+        schema = StructType([
+            StructField("DateTime", StringType()),
+            StructField("RR", StringType()),
+            StructField("SPO2", StringType()),
+            StructField("MAP", StringType()),
+            StructField("SBP", StringType()),
+            StructField("DBP", StringType()),
+            StructField("HR", StringType()),
+            StructField("PP", StringType()),
+            StructField("CO", StringType())
+        ])
+        return schema
+       
+if __name__ == "__main__":
+    
+    # create Spark session
+    spark = SparkSession.builder.master('local').config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0").getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+    
+
+    df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", 'localhost:9092') \
+        .option("subscribe", "Topic_test") \
+        .option("failOnDataLoss", "false") \
+        .option("startingOffsets", "earliest") \
+        .load() 
+
+
+    df.printSchema()
+
+    df = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+    df = df.withColumn("data", from_json(df.value, BloodPressure.get_schema())).select("data.*")
+
+    df.printSchema()
+
+
+## TRAINING THE MODEL
 
 # Read your data into a DataFrame
 data = spark.read.csv(r"C:\Users\User\Desktop\Quadramind-pyspark (1)\Quadramind-pyspark\Kafka\data\historical_data.csv", header=True, inferSchema=True)
@@ -56,9 +106,6 @@ model = kmeans.fit(assembled_data)
 # Make predictions
 predictions = model.transform(assembled_data)
 
-
-# Assuming the 'model' variable holds the trained KMeans model
-model.write().overwrite().save(r"C:\Users\User\Desktop\Quadramind-pyspark (1)\Quadramind-pyspark\PySpark\kmeans2")
 # Show the predictions
 predictions.show()
 
@@ -98,4 +145,56 @@ plt.ylabel("DBP")
 plt.title("K-means Clustering (Colors by Labels)")
 plt.colorbar(label='Labels')
 plt.show()
+
+
+
+
+## REAL TIME STREAMING ANOMALY DETECTION
+
+# Selecting 'DateTime', 'SBP', and 'DBP' columns from the streaming DataFrame (df)
+selected_df = df.select('DateTime', 'SBP', 'DBP')
+
+# Convert string columns to numeric types
+selected_df = selected_df.withColumn('SBP', selected_df['SBP'].cast('double'))
+selected_df = selected_df.withColumn('DBP', selected_df['DBP'].cast('double'))
+
+# Drop rows with null values in 'SBP' or 'DBP' columns
+selected_df = selected_df.dropna(subset=['SBP', 'DBP'])
+
+# Assemble 'SBP' and 'DBP' columns into a single 'features' column
+assembler = VectorAssembler(inputCols=['SBP', 'DBP'], outputCol='features')
+transformed_df = assembler.transform(selected_df)
+
+# Apply the loaded KMeans model to the transformed DataFrame
+streaming_predictions = model.transform(transformed_df)
+
+streaming_predictions.printSchema()
+
+
+assert type(df) == pyspark.sql.dataframe.DataFrame
+row_df = streaming_predictions.select(
+        to_json(struct("Datetime")).alias('key'),
+        to_json(struct('DateTime',  'SBP', 'DBP', 'prediction')).alias("value") )
+    
+    # Write final result into console for debugging purpose
+query = row_df \
+        .writeStream \
+        .trigger(processingTime='30 seconds') \
+        .outputMode("update") \
+        .option("truncate", "false")\
+        .format("console") \
+        .start()
+# Writing to Kafka topic processedBPprediction
+query = row_df\
+        .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+        .writeStream\
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", 'localhost:9092') \
+        .option("topic", output_topic) \
+        .option("checkpointLocation", "checkpoints") \
+        .start()
+
+query.awaitTermination()
+spark.stop()
+
 
